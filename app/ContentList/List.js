34ea27b05ba1ea2d6d46846fc342d42b15148d06
young-utf <young.utf@gmail.com>
2015-08-07 01:00:07 +0900

    List Item Added

--- app/ContentList/List.js
+++ app/ContentList/List.js
@@ -10,14 +10,31 @@ var List = React.createClass({
         };
     },
     componentDidMount: function () {
+        var that = this;
         $.get(API_URL, function (data) {
-            console.log(data);
+            that.state.list = data.data;
         });
     },
     render: function () {
+        var list = this.state.list;
         return (
             <Banner/>
-
+            {
+                list.map(function (data) {
+                    var style = {
+                        "background-image": 'url(' + data.image_url + ')'
+                    };
+                    return (
+                        <a className="magazineItem" href="./view.php?id=" + {data.id} style={style}>
+                            <div className="magazineItemGradient">
+                                <div className="magazineItemText">
+                                    {data.text}
+                                </div>
+                            </div>
+                        </a>
+                    );
+                })
+            }
         );
     }
 });
