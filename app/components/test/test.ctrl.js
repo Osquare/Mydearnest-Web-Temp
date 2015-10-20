e52b7f2b9f34a2dbd614680375da0ffa66440089
young-utf <young.utf@gmail.com>
2015-10-20 19:58:21 +0900

    test

--- app/components/test/test.ctrl.js
+++ app/components/test/test.ctrl.js
@@ -5,6 +5,31 @@
 export default (ngModule) => {
     ngModule.controller('TESTController',
         function ($scope, Methods) {
-            location.href = 'http://deeplink.me/ggumim.co.kr';
+
+            $scope.iosLocation = function () {
+                window.location = "mydearnest://view?msgType=19&id=318002";
+            };
+
+            $scope.androidLocation = function () {
+                window.location = "intent://view?msgType=19&id=318002#Intent;scheme=mydearnest;package=com.osquare.mydearnest;end";
+            };
+
+
+            $scope.iosLocationHref = function () {
+                window.location.href = "mydearnest://view?msgType=19&id=318002";
+            };
+
+            $scope.androidLocationHref = function () {
+                window.location.href = "intent://view?msgType=19&id=318002#Intent;scheme=mydearnest;package=com.osquare.mydearnest;end";
+            };
+
+            $scope.iosLocationAssign = function () {
+                window.location.assign("mydearnest://view?msgType=19&id=318002");
+            };
+
+            $scope.androidLocationAssign = function () {
+                window.location.assign("intent://view?msgType=19&id=318002#Intent;scheme=mydearnest;package=com.osquare.mydearnest;end");
+            };
+
         });
 };
