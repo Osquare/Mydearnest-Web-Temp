b78662f4536d50f9627df1a8013538c65a142863
young-utf <young.utf@gmail.com>
2015-10-20 18:47:27 +0900

    link test

--- app/components/test/test.ctrl.js
+++ app/components/test/test.ctrl.js
@@ -5,14 +5,6 @@
 export default (ngModule) => {
     ngModule.controller('TESTController',
         function ($scope, Methods) {
-
-            $scope.iosLocation = function () {
-                window.location = "mydearnest://view?msgType=19&id=318002";
-            };
-
-            $scope.androidLocation = function () {
-                window.location = "intent://view?msgType=19&id=318002#Intent;scheme=mydearnest;package=com.osquare.mydearnest;end";
-            };
-
+            location.href = 'http://deeplink.me/ggumim.co.kr';
         });
 };
