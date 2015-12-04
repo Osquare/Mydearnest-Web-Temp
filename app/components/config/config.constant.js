7c529088fec92dae39af097d4b23c7282574b6a4
young-utf <young.utf@gmail.com>
2015-12-05 05:51:17 +0900

    change count path

--- app/components/config/config.constant.js
+++ app/components/config/config.constant.js
@@ -7,7 +7,7 @@ module.exports = function (ngModule) {
         API_URL: 'http://api.ggumim.co.kr/1.7/magazines',
         GET_FURNITURE: 'http://api.ggumim.co.kr/1.7/furnitures/',
         IMAGE_URL:  'http://image.ggumim.co.kr/unsafe/{id}/{id}',
-        PageCount_URL:  'http://api.ggumim.co.kr/api/count/',
+        PageCount_URL:  'http://api.ggumim.co.kr/admin/count/',
         iMarket: 'itms-apps://itunes.apple.com/kr/app/id992731402?mt=8',
         AndMarket: 'market://details?id=com.osquare.mydearnest'
     });
